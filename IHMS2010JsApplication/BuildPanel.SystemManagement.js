ff0f02b33481a2107e1719054be14a0dbfc05332
simpkan <simpkan@gmail.com>
2010-04-27 14:47:27 +0000

    调试单建机构信息Window

--- IHMS2010JsApplication/BuildPanel.SystemManagement.js
+++ IHMS2010JsApplication/BuildPanel.SystemManagement.js
@@ -39,19 +39,19 @@ IHMSModule.SystemManagementPanel = Ext.extend(Ext.app.Module,
 function BuildSystemManagementPanel() {
 
 	var tabs = new Ext.TabPanel({
-			iconCls: 'icon-SystemManagementPanel',
-			border: false,
-			autoHeight: true,
-			activeTab: 0,
-			frame:true,
-			defaults:{autoHeight: true},
-			items:[
-				BuildSystemManagementPanel_BranchManagerTreeGrid(),
-				BuildSystemManagementPanel_WorkerManagerGrid(),
-				BuildSystemManagementPanel_GroupAndRoleManagePanel(),
-				BuildSystemManagementPanel_AdministratorPanel()
-			]
-		});
+		iconCls: 'icon-SystemManagementPanel',
+		border: false,
+		autoHeight: true,
+		activeTab: 0,
+		frame:true,
+		defaults:{autoHeight: true},
+		items:[
+			BuildSystemManagementPanel_BranchManagerTreeGrid(),
+			BuildSystemManagementPanel_WorkerManagerGrid(),
+			BuildSystemManagementPanel_GroupAndRoleManagePanel(),
+			BuildSystemManagementPanel_AdministratorPanel()
+		]
+	});
 
 	return tabs;
 }
@@ -59,14 +59,79 @@ function BuildSystemManagementPanel() {
 
 function BuildSystemManagementPanel_BranchManagerTreeGrid() {
 	
+	var menuItem = getMenuItem();
+    var treegrid = new Ext.ux.tree.TreeGrid({
+		iconCls: 'icon-SystemManagementPanel',
+		border: false,
+        width: 500,
+        autoHeight: true,
+        enableDD: true,
+
+        columns:[
+		{
+            dataIndex: 'companyId',
+            hidden: true
+        },{
+            header: '机构名称',
+            dataIndex: 'companyName',
+            width: 300
+        },{
+            header: '机构编码',
+            width: 100,
+            dataIndex: 'companyNo',
+            align: 'center'
+        },{
+            header: '营业厅联系人',
+            width: 100,
+            dataIndex: 'companyManager'
+        },{
+            header: '联系电话',
+            width: 100,
+            dataIndex: 'companyPhone'
+        },{
+			id: 'companyAddress',
+            header: '地址',
+            dataIndex: 'companyAddress'
+        }],
+
+		tbar: [
+			menuItem
+		],
+		
+		bbar: [
+			{text: '共20机构'}
+		],
+		
+        //dataUrl: 'company.json',
+        autoExpandColumn: 'companyAddress'
+
+    });
+	
+	function getMenuItem() {
+		var menuitem = new Ext.menu.Item({
+			id: 'systemManagementAddCompany',
+			text: '新增机构',
+			iconCls: 'icon-StatisticsDataButton'// 'menu' + n.id + "-icon"
+		});
+		
+		menuitem.on("click", //定义菜单项的点击事件
+			function() { 
+				var win = BuildCompanyInfoWindow('新建');
+				win.show();
+			}
+		);
+		return menuitem;
+	}
+	
 	var gridPanel = new Ext.Panel({
 		iconCls: 'icon-SystemManagementPanel',
 		title: '机构管理',
-		html: '维护一个机构的TreeGrid，可以增删改查'
+		border: false,
+		layout: 'fit',
+		items: [ treegrid ]
 	});
 	
 	return gridPanel;
-	
 }
 
 function BuildSystemManagementPanel_WorkerManagerGrid() {
@@ -74,6 +139,7 @@ function BuildSystemManagementPanel_WorkerManagerGrid() {
 	var gridPanel = new Ext.Panel({
 		iconCls: 'icon-SystemManagementPanel',
 		title: '用户管理',
+		layout: 'fit',
 		html: '维护一个所有柜员的Grid，可以增删改查'
 	});
 	
@@ -86,6 +152,7 @@ function BuildSystemManagementPanel_GroupAndRoleManagePanel() {
 	var gridPanel = new Ext.Panel({
 		iconCls: 'icon-SystemManagementPanel',
 		title: '用户组/角色/权限管理',
+		layout: 'fit',
 		html: '维护用户组/角色/权限管理的面板，可以增删改查'
 	});
 	
@@ -98,6 +165,7 @@ function BuildSystemManagementPanel_AdministratorPanel() {
 	var gridPanel = new Ext.Panel({
 		iconCls: 'icon-SystemManagementPanel',
 		title: '管理员维护',
+		layout: 'fit',
 		html: '包括参数维护，日志维护，备份等功能'
 	});
 	
---
+++ IHMS2010JsApplication/IHMSControl.Window.CompanyInfo.js
@@ -0,0 +1,37 @@
+﻿
+var companyInfoWindow = null;
+function BuildCompanyInfoWindow ( flag ) {
+	if (jQuery.isEmptyObject(companyInfoWindow)) {
+		companyInfoWindow = new Ext.Window(
+		{
+			title: '机构信息',
+			closable: false,
+			width: 420,
+			height: 460,
+			plain:true,
+			layout: 'fit',
+			// items: 
+			// [
+			// ],
+			
+			buttons: 
+			[
+				{
+					text: flag,
+					handler: function(){ 
+						companyInfoWindow.close();
+						companyInfoWindow = null;
+					}
+				},
+				{
+					text: '取消',
+					handler: function(){
+						companyInfoWindow.close(); 
+						companyInfoWindow = null;
+					}
+				}
+			]
+		});
+	}
+	return companyInfoWindow;
+}
