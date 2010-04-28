a06ae65394c280304b7885eadcd423a7b73c59e7
simpkan@gmail.com <simpkan@gmail.com@d4f5e7d2-ff40-11de-87c3-653bb6514e15>
2010-04-28 04:03:32 +0000

    增加了Guid的生成的JQuery文件。

--- trunk/IHMS2010JsApplication/IHMSControl.Window.CompanyInfo.js
+++ trunk/IHMS2010JsApplication/IHMSControl.Window.CompanyInfo.js
@@ -1,37 +1,80 @@
 ﻿
 var companyInfoWindow = null;
 function BuildCompanyInfoWindow ( flag ) {
-	if (jQuery.isEmptyObject(companyInfoWindow)) {
-		companyInfoWindow = new Ext.Window(
+
+    var form = new Ext.form.FormPanel({
+        baseCls: 'x-plain',
+		//labelAlign: 'right',
+        labelWidth: 90,
+        //url:'save-form.php',
+        defaultType: 'textfield',
+
+        items: [
 		{
-			title: '机构信息',
-			closable: false,
-			width: 420,
-			height: 460,
-			plain:true,
+            fieldLabel: '机构ID',
+			readOnly: true,
+			//style: 'background-color: gray',
+            name: 'to',
+			disable: false,
+			value: jQuery.Guid.New(),
+            anchor:'100%'  // anchor width by percentage
+        },{
+            fieldLabel: '上级机构',
+			xtype: 'combo',
+            name: 'subject',
+            anchor: '100%'  // anchor width by percentage
+        },{
+            fieldLabel: '机构名称',
+            name: 'subject',
+            anchor: '100%'  // anchor width by percentage
+        },{
+            fieldLabel: '机构编码',
+            name: 'subject',
+            anchor: '100%'  // anchor width by percentage
+        },{
+            fieldLabel: '营业厅联系人',
+            name: 'subject',
+            anchor: '100%'  // anchor width by percentage
+        },{
+            fieldLabel: '联系电话',
+            name: 'subject',
+            anchor: '100%'  // anchor width by percentage
+        },{
+            fieldLabel: '机构地址',
+			name: 'msg',
+            anchor: '100%'  // anchor width by percentage and height by raw adjustment
+        }]
+    });
+
+	if (jQuery.isEmptyObject(companyInfoWindow)) {
+		companyInfoWindow = new Ext.Window({
+			title: flag + ' 机构信息',
+			width: 480,
+			height:280,
+			minWidth: 300,
+			minHeight: 200,
 			layout: 'fit',
-			// items: 
-			// [
-			// ],
-			
-			buttons: 
-			[
-				{
-					text: flag,
-					handler: function(){ 
-						companyInfoWindow.close();
-						companyInfoWindow = null;
-					}
-				},
-				{
-					text: '取消',
-					handler: function(){
-						companyInfoWindow.close(); 
-						companyInfoWindow = null;
-					}
+			plain:true,
+			bodyStyle:'padding:5px;',
+			//buttonAlign:'center',
+			items: form,
+
+			buttons: [{
+				text: '确定',
+				handler: function(){ 
+					companyInfoWindow.close();
+					companyInfoWindow = null;
 				}
-			]
+			},{
+				text: '取消',
+				handler: function(){ 
+					companyInfoWindow.close();
+					companyInfoWindow = null;
+				}
+			}]
 		});
 	}
+
 	return companyInfoWindow;
+
 }
