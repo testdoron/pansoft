5f65719b0ac95b9c99e7546d0484827e2f85a771
simpkan <simpkan@gmail.com>
2010-04-29 04:23:06 +0000

    参数管理面板基本完成

---
+++ IHMS2010JsApplication/IHMSControl.Grid.Admin.js
@@ -0,0 +1,66 @@
+﻿
+
+
+function BuildAdminPanel () {
+
+	
+    var myTabPanel = new Ext.TabPanel ({
+	
+		height: 448,
+		border: false,
+		activeTab: 0,
+		bbar: [ '状态' ],
+		items: [ getParameterManagerPanel(), getLogManagerPanel(), getBackupManagerPanel(), getAboutPanel()]
+	
+	})
+	
+	return myTabPanel;
+	
+	//参数管理
+	function getParameterManagerPanel() {
+	
+		var myForm = new Ext.Panel({
+			title: "参数管理",
+			border: false,
+			bbar:['参数管理']
+			//standardSubmit: true,
+			//items: myFieldset
+		});
+		return myForm;
+	
+	}
+	
+	//日志管理
+	function getLogManagerPanel() {
+	
+		var myForm = new Ext.grid.GridPanel({
+			title: "日志管理",
+			border: false
+		});
+		return myForm;
+
+	}
+
+	//备份管理
+	function getBackupManagerPanel() {
+	
+		var myForm = new Ext.grid.GridPanel({
+			title: "备份管理",
+			border: false
+		});
+		return myForm;
+
+	}
+
+	//关于
+	function getAboutPanel() {
+	
+		var myForm = new Ext.Panel({
+			title: "关于",
+			border: false
+		});
+		return myForm;
+
+	}
+	
+}
